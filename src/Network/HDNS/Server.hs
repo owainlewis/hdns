78bfc0f0474ccb4a1366083395060324ca401622
Owain Lewis <owain.lewis@oracle.com>
2019-03-03 16:04:17 +0000

    Update Server.hs

--- src/Network/HDNS/Server.hs
+++ src/Network/HDNS/Server.hs
@@ -26,14 +26,3 @@ start = withSocketsDo $ do
     forever $ do
       (conn, peer) <- accept sock
       putStrLn $ "Connection from " ++ show peer
-
---
---
---loop sock conn def 0
-  -- where
-  --   loop sock conn def' n = do
-  --     (bs, addr) <- recvFrom sock (bufSize def')
-  --     _ <- forkIO $ handlePacket conf def' conn sock addr bs n
-  --     case n of
-  --       255 -> loop sock conn def' 0
-  --       _ -> loop sock conn def' (n+1)
